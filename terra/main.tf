36dfa130917c33599bdef1c97ae1c57839312194
muni2 <munisekhar.reddy@gmail.com>
2019-10-27 12:27:20 -0400

    write state file in a directory

--- terra/main.tf
+++ terra/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "munitestbucket1"
-    key    = "terraform.tfstate"
+    key    = "tfstatefiles/terraform.tfstate"
     region = "us-east-2"
   }
 }
